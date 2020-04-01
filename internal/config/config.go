a35b7af8143421f9f18650a40c44c7e334afc046
Salyakhov Arthur <artur@addreality.com>
2020-04-01 11:25:59 +0300

    changed the names of config environment variables

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,27 +9,27 @@ type Config struct {
 }
 
 type Store struct {
-	Postgres PostgresConf `envconfig:"POSTGRES"`
-	Redis    RedisConf    `envconfig:"REDIS"`
+	Postgres PostgresConf
+	Redis    RedisConf
 }
 
 type PostgresConf struct {
-	Host     string `envconfig:"P_HOST" required:"false" default:"127.0.0.1"`
-	Database string `envconfig:"P_DATABASE" required:"false" default:"crm"`
-	Port     string `envconfig:"P_PORT" required:"false" default:"5432"`
-	User     string `envconfig:"P_USER" required:"false" default:"postgres"`
-	Password string `envconfig:"P_PASSWORD" required:"false" default:"password"`
+	Host     string `default:"127.0.0.1"`
+	Database string `default:"crm"`
+	Port     string `default:"5432"`
+	User     string `default:"postgres"`
+	Password string `default:"password"`
 }
 
 type RedisConf struct {
-	Host     string `envconfig:"R_HOST" required:"false" default:"127.0.0.1:6379"`
-	Password string `envconfig:"R_PASSWORD" required:"false" default:""`
-	DB       int    `envconfig:"R_DB" required:"false" default:"0"`
+	Host     string `default:"127.0.0.1:6379"`
+	Password string `default:""`
+	DB       int    `default:"0"`
 }
 
 func New() (*Config, error) {
 	var c Config
-	if err := envconfig.Process("AUTHONE", &c); err != nil {
+	if err := envconfig.Process("qilin", &c); err != nil {
 		return nil, err
 	}
 
