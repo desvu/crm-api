bbe0ec7c31b5575794aee615ea48d419e91bf270
Serge <serge@cheebo.me>
2020-04-30 19:56:48 +0500

    feat: system requirement grpc

--- internal/handler/grpc/game/game.go
+++ internal/handler/grpc/game/game.go
@@ -61,5 +61,29 @@ func (h Handler) GetByIDAndRevisionID(ctx context.Context, stream *proto.Request
 		})
 	}
 
+	for _, item := range game.Revision.SystemRequirements {
+		r := &proto.SystemRequirements{
+			Platform: item.Platform.String(),
+		}
+		if item.Minimal != nil {
+			r.Minimal = &proto.RequirementsSet{
+				CPU:       item.Minimal.CPU,
+				GPU:       item.Minimal.GPU,
+				DiskSpace: uint32(item.Minimal.DiskSpace),
+				RAM:       uint32(item.Minimal.RAM),
+			}
+		}
+		if item.Recommended != nil {
+			r.Recommended = &proto.RequirementsSet{
+				CPU:       item.Recommended.CPU,
+				GPU:       item.Recommended.GPU,
+				DiskSpace: uint32(item.Recommended.DiskSpace),
+				RAM:       uint32(item.Recommended.RAM),
+			}
+		}
+
+		result.SystemRequirements = append(result.SystemRequirements, r)
+	}
+
 	return result, nil
 }
