082383187b1439122cdf61ac164a882fe4495dc0
Aleksandr <aleksandr.barsukov@protocol.one>
2020-05-08 14:04:37 +0300

    reg handler, empty active storefront

--- internal/app/container/handler/container.go
+++ internal/app/container/handler/container.go
@@ -1,18 +1,14 @@
 package handler
 
 import (
-	"github.com/qilin/crm-api/internal/handler/grpc"
-	"github.com/qilin/crm-api/internal/handler/grpc/game"
 	"github.com/qilin/crm-api/internal/handler/http"
 	"github.com/qilin/crm-api/internal/handler/http/storefront"
 	"go.uber.org/fx"
 )
 
-func New() fx.Option {
+func NewHttp() fx.Option {
 	return fx.Provide(
-		storefront.NewHandler,
-		game.New,
-		grpc.New,
 		http.New,
+		storefront.NewHandler,
 	)
 }
---
+++ internal/app/container/handler/grpc.go
@@ -0,0 +1,16 @@
+package handler
+
+import (
+	"github.com/qilin/crm-api/internal/handler/grpc"
+	"github.com/qilin/crm-api/internal/handler/grpc/game"
+	"github.com/qilin/crm-api/internal/handler/grpc/storefront"
+	"go.uber.org/fx"
+)
+
+func NewGrpc() fx.Option {
+	return fx.Provide(
+		grpc.New,
+		game.New,
+		storefront.New,
+	)
+}
